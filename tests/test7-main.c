68c69198fac3f17204bac75133135456ab8bcf87
Dodji Seketeli <dodji@src.gnome.org>
2003-06-08 13:48:04 +0000

    *** empty log message ***

--- tests/test7-main.c
+++ tests/test7-main.c
@@ -85,102 +85,100 @@ const guchar *gv_cssbuf =
 */
 
 const guchar *gv_xmlbuf =
-"<?xml version=\"1.0\"?>
-<rss version=\"0.91\">
-  <channel>
-    <title>scottandrew.com JavaScript and DHTML Channel</title>
-<link>http://www.scottandrew.com</link>
-<description>DHTML, DOM and JavaScript snippets from scottandrew.com</description>
-    <language>en-us</language>
-    <item>
-      <title>DHTML Animation Array Generator</title>
-<description>Robert points us to the first third-party tool for the DomAPI: The Animation Array Generator, a visual tool for creating...</description>
-<link>http://www.scottandrew.com/weblog/2002_06#a000395</link>
-    </item>
-    <item>
-      <title>DOM and Extended Entries</title>
-<description>Aarondot: A Better Way To Display Extended Entries. Very cool, and uses the DOM and JavaScript to reveal the extended...</description>
-<link>http://www.scottandrew.com/weblog/2002_06#a000373</link>
-    </item>
-    <item>
-      <title>cellspacing and the DOM</title>
-<description>By the way, if you're using the DOM to generate TABLE elements, you have to use setAttribute() to set the...</description>
-      <link>http://www.scottandrew.com/weblog/2002_05#a000365</link>
-    </item>
-    <item>
-      <title>contenteditable for Mozilla</title>
-      <description>The folks art Q42, creator of Quek (cute little avatar/chat) and Xopus (browser-based WYSIWYG XML-editor) have released code that simulates...</description>
-      <link>http://www.scottandrew.com/weblog/2002_05#a000361</link>
-    </item>
-  </channel>
-</rss>
-";
+"<?xml version=\"1.0\"?>"
+"<rss version=\"0.91\">"
+"  <channel>"
+"    <title>scottandrew.com JavaScript and DHTML Channel</title>"
+"<link>http://www.scottandrew.com</link>"
+"<description>DHTML, DOM and JavaScript snippets from scottandrew.com</description>"
+"    <language>en-us</language>"
+"    <item>"
+"      <title>DHTML Animation Array Generator</title>"
+"<description>Robert points us to the first third-party tool for the DomAPI: The Animation Array Generator, a visual tool for creating...</description>"
+"<link>http://www.scottandrew.com/weblog/2002_06#a000395</link>"
+"    </item>"
+"    <item>"
+"      <title>DOM and Extended Entries</title>"
+"<description>Aarondot: A Better Way To Display Extended Entries. Very cool, and uses the DOM and JavaScript to reveal the extended...</description>"
+"<link>http://www.scottandrew.com/weblog/2002_06#a000373</link>"
+"    </item>"
+"    <item>"
+"      <title>cellspacing and the DOM</title>"
+"<description>By the way, if you're using the DOM to generate TABLE elements, you have to use setAttribute() to set the...</description>"
+"      <link>http://www.scottandrew.com/weblog/2002_05#a000365</link>"
+"    </item>"
+"    <item>"
+"      <title>contenteditable for Mozilla</title>"
+"      <description>The folks art Q42, creator of Quek (cute little avatar/chat) and Xopus (browser-based WYSIWYG XML-editor) have released code that simulates...</description>"
+"      <link>http://www.scottandrew.com/weblog/2002_05#a000361</link>"
+"    </item>"
+"  </channel>"
+"</rss>"
+;
 
 const char * gv_cssbuf=
-"
-rss
-{
-    display:block;
-    margin:10px;
-}
-
-channel
-{
- display:block;
- /*height:300px;*/
- width:200px;
- border:1px solid #000;
- overflow:auto;
- background-color:#eee;
- font: 12px verdana;
-}
-
-item
-{
- display: block;
- padding:10px;
- margin-bottom:10px;
- border:1px solid #ccc;
- /*border-top:1px solid #ccc;
- border-bottom:1px solid #ccc;
- border-left:1px solid #ccc;
- border-right:1px solid #ccc;
- */
- background-color:#fff;
-}
-
-
-channel>title, channel>description
-{
-        display: block;
-        margin-left:10px;
-        margin-top:10px;
-        background-color:#eee;
-        font-weight:bold;
-}
-
-channel>title
-{
-        font-size:16px;
-}
-
-channel>description
-{
-        display: block ;
-        font-size:10px;
-        margin-bottom:10px;
-}
-
-item>title
-{
-        font-weight:bold;
-}
-
-item>link, channel>link, channel>language
-{
-        display: none;
-}
-"
+"rss"
+"{                         "
+"    display:block;        "
+"    margin:10px;          "
+"}                         "
+"                          "
+"channel                   "
+"{                         "
+" display:block;           "
+" /*height:300px;*/        "
+" width:200px;             "
+" border:1px solid #000;   "
+" overflow:auto;           "
+" background-color:#eee;   "
+" font: 12px verdana;      "
+"}                         "
+"                          "
+"item                      "
+"{                         "
+" display: block;          "
+" padding:10px;            "
+" margin-bottom:10px;      "
+" border:1px solid #ccc;   "
+" /*border-top:1px solid #c"
+" border-bottom:1px solid #"
+" border-left:1px solid #cc"
+" border-right:1px solid #c"
+" */                       "
+" background-color:#fff;   "
+"}                         "
+"                          "
+"                          "
+"channel>title, channel>des"
+"{                         "
+"        display: block;   "
+"        margin-left:10px; "
+"        margin-top:10px;  "
+"        background-color:#"
+"        font-weight:bold; "
+"}                         "
+"                          "
+"channel>title             "
+"{                         "
+"        font-size:16px;   "
+"}                         "
+"                          "
+"channel>description       "
+"{                         "
+"        display: block ;  "
+"        font-size:10px;   "
+"        margin-bottom:10px"
+"}                         "
+"                          "
+"item>title                "
+"{                         "
+"        font-weight:bold; "
+"}                         "
+"                          "
+"item>link, channel>link,  "
+"{                         "
+"        display: none;    "
+"}                         "  
 ;
 
 /*
