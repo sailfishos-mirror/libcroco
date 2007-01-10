ef258807ec53ed9639dd68e897de98dbe81f408c
Bruno Haible <bruno@clisp.org>
2007-01-10 18:14:16 +0000

    Handle FONT_WEIGHT_INHERIT.
    2007-01-10  Bruno Haible  <bruno@clisp.org>
    
            * cr-fonts.c (cr_font_weight_get_bolder): Handle FONT_WEIGHT_INHERIT.
    
    
    svn path=/trunk/; revision=311

--- src/cr-fonts.c
+++ src/cr-fonts.c
@@ -766,7 +766,10 @@ cr_font_variant_to_string (enum CRFontVariant a_code)
 enum CRFontWeight
 cr_font_weight_get_bolder (enum CRFontWeight a_weight)
 {
-        if (a_weight >= FONT_WEIGHT_900) {
+        if (a_weight == FONT_WEIGHT_INHERIT) {
+                cr_utils_trace_info ("can't return a bolder weight for FONT_WEIGHT_INHERIT") ;
+                return a_weight;
+        } else if (a_weight >= FONT_WEIGHT_900) {
                 return FONT_WEIGHT_900 ;
         } else if (a_weight < FONT_WEIGHT_NORMAL) {
                 return FONT_WEIGHT_NORMAL ;
