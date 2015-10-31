64e310323ae6bebc7da62c05590d34f198b8bf43
Ignacio Casal Quinteiro <icq@gnome.org>
2015-10-31 11:40:36 +0100

    Renew autogen to not use gnome-autogen

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+olddir=`pwd`
 
-PKG_NAME="libcroco"
+cd $srcdir
 
-REQUIRED_AUTOMAKE_VERSION=1.7
-
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/README \
-  && test -f $srcdir/src/cr-parser.h) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
+(test -f configure.ac) || {
+	echo "*** ERROR: Directory "\`$srcdir\'" does not look like the top-level project directory ***"
+	exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
-}
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' configure.ac`
 
-if test $# -eq 0 ; then
-    default_args="--enable-tests=yes --enable-seleng=auto --enable-layeng=auto"
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+	echo "*** WARNING: I am going to run \`configure' with no arguments." >&2
+	echo "*** If you wish to pass any to it, please specify them on the" >&2
+	echo "*** \`$0\' command line." >&2
+	echo "" >&2
 fi
 
-REQUIRED_AUTOMAKE_VERSION=1.7.2
-USE_GNOME2_MACROS=1 . gnome-autogen.sh $default_args
+aclocal --install || exit 1
+gtkdocize --copy || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+
+cd $olddir
+if [ "$NOCONFIGURE" = "" ]; then
+	$srcdir/configure "$@" || exit 1
+
+	if [ "$1" = "--help" ]; then exit 0 else
+		echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+	fi
+else
+	echo "Skipping configure process."
+fi
