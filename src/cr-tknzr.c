sailfishos-mirror/libcroco

commit bc9b2c339e1181f8c69a2f74a93876932e352d6d

Fix cr_tknzr_consume_chars() to set *a_nb_char before returning
https://bugzilla.gnome.org/show_bug.cgi?id=710076

src/cr-tknzr.c
@@ -1901,6 +1901,8 @@ cr_tknzr_seek_index (CRTknzr * a_this, enum CRSeekPos a_origin, gint a_pos)
 enum CRStatus
 cr_tknzr_consume_chars (CRTknzr * a_this, guint32 a_char, glong * a_nb_char)
 {
+	gulong consumed;
+	enum CRStatus status;
         g_return_val_if_fail (a_this && PRIVATE (a_this)
                               && PRIVATE (a_this)->input, CR_BAD_PARAM_ERROR);
 
@@ -1911,8 +1913,10 @@ cr_tknzr_consume_chars (CRTknzr * a_this, guint32 a_char, glong * a_nb_char)
                 PRIVATE (a_this)->token_cache = NULL;
         }
 
-        return cr_input_consume_chars (PRIVATE (a_this)->input,
-                                       a_char, a_nb_char);
+        status = cr_input_consume_chars (PRIVATE (a_this)->input,
+                                         a_char, &consumed);
+	*a_nb_char = (glong) consumed;
+	return status;
 }
 
 enum CRStatus

commit f4f9e6d897bcb7dff768dda66a9e62c80137d7f5

Fix regression in cr_tknzr_consume_chars()
Commit bc9b2c339e introduced an intermediate variable to account
for the difference in signedness between cr_tknzr_consume_chars()
and cr_input_consume_chars(), but missed that the variable in
question is an in-out variable that requires proper initialization.

https://bugzilla.gnome.org/show_bug.cgi?id=759565

src/cr-tknzr.c
@@ -1901,7 +1901,7 @@ cr_tknzr_seek_index (CRTknzr * a_this, enum CRSeekPos a_origin, gint a_pos)
 enum CRStatus
 cr_tknzr_consume_chars (CRTknzr * a_this, guint32 a_char, glong * a_nb_char)
 {
-	gulong consumed;
+	gulong consumed = *(gulong *) a_nb_char;
 	enum CRStatus status;
         g_return_val_if_fail (a_this && PRIVATE (a_this)
                               && PRIVATE (a_this)->input, CR_BAD_PARAM_ERROR);